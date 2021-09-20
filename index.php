b2f2e1b71e6b400d247b824b59ee98ca632aa054
ClaudeJ <myprojects@claudej.dev>
2021-09-20 08:23:30 +0200

    Alertes et sessions

--- index.php
+++ index.php
@@ -1,34 +1,36 @@
 <?php
 
-require_once("./controllers/mainController.controller.php");
+session_start();
+
+require_once './controllers/mainController.controller.php';
 $mainController = new MainController();
 try {
     if (empty($_GET['page'])) {
-        $page = "home";
+        $page = 'home';
     } else {
-      $url = explode("/", filter_var($_GET['page'],FILTER_SANITIZE_URL));
-      $page = $url[0];
+        $url = explode('/', filter_var($_GET['page'], FILTER_SANITIZE_URL));
+        $page = $url[0];
     }
-    
-    switch ($page) { 
+
+    switch ($page) {
         case 'navbar': $mainController->navbar();
             break;
-        
+
         case 'home': $mainController->home();
             break;
-        
+
         case 'link': $mainController->link();
             break;
-        
+
         case 'action0': $mainController->action0();
             break;
-    
+
         case 'action1': $mainController->action1();
             break;
-    
+
         case 'action2': $mainController->action2();
             break;
-        default : throw new Exception("Error 404 : Houston, we have a problem", 1);
+        default: throw new Exception('Error 404 : Houston, we have a problem', 1);
     }
 } catch (Exception $e) {
     $mainController->errorPage($e->getMessage());
--- views/common/template.php
+++ views/common/template.php
@@ -4,13 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?= $page_description ?>">
-    <title><?= $page_title ?></title>
+    <meta name="description" content="<?php echo $page_description; ?>">
+    <title><?php echo $page_title; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
 <body>
-    <?php require_once("views/common/menu.php"); ?>
-    <?= $page_content ?> <!-- equivalent à < ?php echo -->
+    <?php require_once 'views/common/menu.php'; ?>
+
+    <?php if (!empty($_SESSION['alert'])) { ?> <!-- l'alerte est affichée si elle est remplie dans la variable de session -->
+        <div class="alert <?php echo $_SESSION['alert']['type']; ?>" role="alert">
+            <?php echo $_SESSION['alert']['message']; ?>
+        </div>
+    <?php unset($_SESSION['alert']); //l'alerte est supprimée lorsqu'elle a été affichée une fois
+        }
+    ?>
+
+    <?php echo $page_content; ?> <!-- equivalent à < ?php echo -->
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </body>
